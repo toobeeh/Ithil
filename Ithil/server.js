483746227cef53bf9f65ffccba21d4c481e76328
tobeh <tobiasch@gmx.at>
2021-01-18 20:47:25 +0100

    rem helmet

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,7 +1,6 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const helmet = require("helmet");
 
 //app.get('/', (req, res) => {
 //    res.sendFile(__dirname + '/index.html');

82401ca4991955fc6799abfd59a396b38fe3a531
tobeh <tobiasch@gmx.at>
2021-01-18 22:31:02 +0100

    idk anymore seriously

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,6 +1,7 @@
-const app = require('express')();
-const io = require('socket.io')({ transports: ['websocket'] });
+
+const server = require('http').createServer();
+const io = require('socket.io')(server);
 io.on('connection', socket => { console.log('a user connected'); });
-app.listen(3000);
+server.listen(3000);
 
 

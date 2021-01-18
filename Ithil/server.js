b58d031d968345acbdaa702c96546238fc3083b5
tobeh <tobiasch@gmx.at>
2021-01-18 21:37:37 +0100

    chnge cors

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,14 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http, {
-    handlePreflightRequest: (req, res) => {
-        res.writeHead(200, {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
-        });
-        res.end();
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["*"],
     }
-});
+}
+);
 
 //app.get('/', (req, res) => {
 //    res.sendFile(__dirname + '/index.html');

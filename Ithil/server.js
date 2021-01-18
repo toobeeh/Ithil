30cb25075f82ed59748997c74677f5baad3daae6
tobeh <tobiasch@gmx.at>
2021-01-18 22:06:43 +0100

    cors without http

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,16 +1,11 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const cors = require("cors");
-app.use(cors());
-//app.get('/', (req, res) => {
-//    res.sendFile(__dirname + '/index.html');
-//});
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-});
+const server = require('http').createServer();
+options = {
+    cors: true,
+    origins: ["*"],
+}
+const io = require('socket.io')(server, options);
+io.on('connection', socket => { console.log('a user connected'); });
+server.listen(3000);
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-});
+

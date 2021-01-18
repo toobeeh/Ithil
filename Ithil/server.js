f16ef7d1999f2b2556da365464c45ac35201e0ad
tobeh <tobiasch@gmx.at>
2021-01-18 21:17:44 +0100

    change cors

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,9 +1,13 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-    origins: ["https://skribbl.io", "https://typo.rip" ]
+const io = require('socket.io')(http);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
 });
-
 //app.get('/', (req, res) => {
 //    res.sendFile(__dirname + '/index.html');
 //});

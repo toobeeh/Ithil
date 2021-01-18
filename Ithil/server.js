55c1c4a1af621fa7cd472fe648864bde2791822a
tobeh <tobiasch@gmx.at>
2021-01-18 22:40:56 +0100

    last try

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,5 +1,5 @@
-
-const server = require('http').createServer();
+const app = require("express")();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 io.on('connection', socket => { console.log('a user connected'); });
 server.listen(3000);

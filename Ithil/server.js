9196708bb1d4352e4916b7efeb4a12a02b71f47c
tobeh <tobiasch@gmx.at>
2021-01-18 18:12:46 +0100

    App use

--- Ithil/server.js
+++ Ithil/server.js
@@ -5,6 +5,14 @@ const io = require('socket.io')(http);
 const port = 3000;
 const host = "0.0.0.0";
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
 http.listen({ host: host, port: port }, () => {
     console.log(`Listening on ${host}:${port}.`)
 });

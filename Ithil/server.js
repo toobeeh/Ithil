279bf4049bc3a925c2ca55ee4801dc9aa0dbc02a
tobeh <tobiasch@gmx.at>
2021-01-18 20:28:03 +0100

    exmple

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,36 +1,15 @@
-'use strict';
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3000;
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
+//app.get('/', (req, res) => {
+//    res.sendFile(__dirname + '/index.html');
+//});
 
-http.listen(port, () => {
-    console.log(`Listening on port:${port}.`)
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
 
-io.of("/socket.io").on("connection", (socket) => {
-    console.log(`sio User connected: ${JSON.stringify(socket)}`);
-    socket.on("disconnect", () => {
-        console.log("He's gone.");
-    });
-});
-io.of("/nodejs").on("connection", (socket) => {
-    console.log(`njs User connected: ${JSON.stringify(socket)}`);
-    socket.on("disconnect", () => {
-        console.log("He's gone.");
-    });
-});
-io.on("connection", (socket) => {
-    console.log(`User connected: ${JSON.stringify(socket)}`);
-    socket.on("disconnect", () => {
-        console.log("He's gone.");
-    });
+http.listen(3000, () => {
+    console.log('listening on *:3000');
 });

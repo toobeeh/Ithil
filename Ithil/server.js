44d5642d2e7000b838ad891d6b474841d6cd00c6
tobeh <tobiasch@gmx.at>
2021-01-19 11:08:39 +0100

    socket

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,4 +1,15 @@
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-server.listen(8000);
-console.log('Server started at port: 8000');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(8000, () => {
+    console.log('listening on *:3000');
+});

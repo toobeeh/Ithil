e2cb1208dfc6cd986fc680ce67cf190e52333064
tobeh <tobiasch@gmx.at>
2021-01-19 10:48:00 +0100

    subdir

--- Ithil/server.js
+++ Ithil/server.js
@@ -4,6 +4,9 @@ const http = require('http').createServer(app);
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
+app.get('/sub', (req, res) => {
+    res.send('<h1>Hello subdirectory</h1>');
+});
 
 http.listen(3000, () => {
   console.log('listening on *:3000');

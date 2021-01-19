aa48dac0bac67c577ee5bb1b622f968bf8e7efa2
tobeh <tobiasch@gmx.at>
2021-01-19 17:31:56 +0100

    ssl

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
 var app = require('express')();
 var https = require('https');
-var server = https.createServer({
+var options = {
     key: fs.readFileSync('/etc/letsencrypt/live/typo.rip/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/typo.rip/cert.pem'),
     ca: fs.readFileSync('/etc/letsencrypt/live/typo.rip/chain.pem'),
-    requestCert: false,
-    rejectUnauthorized: false
-}, app);
-server.listen(3000);
+};
+let server = https.createServer(options, function (req, res) {
+    res.writeHead(200);
+    res.end("hello world\n");
+}).listen(3000);
 
 var io = require('socket.io').listen(server);
 io.on('connection', (socket) => {

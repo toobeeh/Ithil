81e55ae74caa4f3f86cd4498745a8f8675f7518a
tobeh <tobiasch@gmx.at>
2021-01-19 10:54:54 +0100

    sub

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,9 +1,7 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+
 app.get('/sub', (req, res) => {
     res.send('<h1>Hello subdirectory</h1>');
 });

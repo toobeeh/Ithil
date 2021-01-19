c1928f7aa199ce1540113de4f759d94e3721a3ca
tobeh <tobiasch@gmx.at>
2021-01-19 10:46:48 +0100

    new thing

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,7 +1,10 @@
-const app = require("express")();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', socket => { console.log('a user connected'); });
-server.listen(3000);
+const app = require('express')();
+const http = require('http').createServer(app);
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
 
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});

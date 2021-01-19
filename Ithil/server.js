69698aecf9219e5c4eb517521efcbc42687173c8
tobeh <tobiasch@gmx.at>
2021-01-19 18:02:14 +0100

    nexttry

--- Ithil/server.js
+++ Ithil/server.js
@@ -1,15 +1,28 @@
-fs = require('fs');
+var app = require('express')();
+var https = require('https');
+var fs = require('fs');
+// path to certs
+var path = '/etc/letsencrypt/live/typo.rip';
+// create server
+var server = https.createServer({
+    key: fs.readFileSync(path + '/privkey.pem', 'utf8'),
+    cert: fs.readFileSync(path + '/cert.pem', 'utf8'),
+    ca: fs.readFileSync(path + '/chain.pem', 'utf8')
+}, app);
+// start listening
+server.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+// io client
+var io = require('socket.io')(server);
 
-// Options for socket.io > 1.0.0
-var options = {
-    allowUpgrades: true,
-    transports: ['polling', 'websocket'],
-    pingTimeout: 6000,
-    pingInterval: 3000,
-    httpCompression: true,
-    key: fs.readFileSync('/etc/letsencrypt/live/typo.rip/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/typo.rip/fullchain.pem'),
-    origins: '*'
-};
-
-io = require('socket.io')(8000, options);
+// testing connection
+io.on('connection', function (socket) {
+    socket.on('test', function (data) {
+        socket.emit('ackmessage', {
+            'msg': 'data',
+            'key': '222'
+        });
+        console.log('connected');
+    });
+});

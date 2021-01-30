c376266ba56280a506dc0e4775f13b642bf87220
tobeh <tobiasch@gmx.at>
2021-01-30 12:32:52 +0100

    debug

--- Ithil/server.js
+++ Ithil/server.js
@@ -66,7 +66,8 @@ const drops = {
         let nextDrop;
         // wait for next drop to appear, check in 5s intervals
         while ((nextDrop = palantirDb.getDrop()).drop.CaughtLobbyKey != "") await drops.idle(5000);
-        console.log(JSON.stringify(nextDrop.drop));
+        console.log(new Date(nextDrop.drop.ValidFrom));
+        console.log(new Date());
         let ms = (new Date(nextDrop.drop.ValidFrom)).getTime() - Date.now();
         console.log("Next drop in " + ms / 1000 + "s");
         await drops.idle(ms);

3bb82b3cac0b5fb2f0f82897be5a283c3aab9c7e
tobeh <tobiasch@gmx.at>
2021-03-06 00:48:05 +0100

    fix scope

--- Ithil/typoSocket.js
+++ Ithil/typoSocket.js
@@ -93,7 +93,7 @@ class TypoSocket {
         member.member.Guilds.forEach(guild => {
             this.socket.join("guild" + guild.GuildID.slice(0,-2));
         });
-        let flags = (Number(this.member.member.flag) >>> 0).toString(2);
+        let flags = (Number(member.member.flag) >>> 0).toString(2);
         if (flags[flags.length - 4] == "1") { this.riproEnabled = true; console.log("ripro connected");}
         else this.riproEnabled = false;
         this.loginDate = Math.ceil(Date.now());

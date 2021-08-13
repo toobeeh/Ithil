e7fc68c951e302a6eb91768cedbfb26631602ab6
tobeh <tobiasch@gmx.at>
2021-08-13 02:55:00 +0200

    two cpu quads

--- Ithil/pm2-ecosystem.config.js
+++ Ithil/pm2-ecosystem.config.js
@@ -4,10 +4,17 @@ module.exports = {
             name: "Ithil Master Server",
             script: "ithilMaster.js"
         }, {
-            name: "Ithil Worker Server",
+            name: "Ithil Worker Server QUAD 1",
             script: "ithilWorker.js",
             exec_mode: "cluster",
-            instances: 8,
+            instances: 4,
+            wait_ready: true,
+            listen_timeout: 10000
+        }, {
+            name: "Ithil Worker Server QUAD 2",
+            script: "ithilWorker.js",
+            exec_mode: "cluster",
+            instances: 4,
             wait_ready: true,
             listen_timeout: 10000
         }]

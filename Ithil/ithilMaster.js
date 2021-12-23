b12293a209e8c15af1f15bc402e4499fce9106c0
tobeh <tobiasch@gmx.at>
2021-12-24 00:01:23 +0100

    fix timestamp

--- Ithil/ithilMaster.js
+++ Ithil/ithilMaster.js
@@ -233,7 +233,7 @@ class Drops {
                         // send all claims after a while
                         setTimeout(() => {
                             if (lastProcessedClaim) {
-                                let ranks = [(lastProcessedClaim.username + " claimed after " + nextDrop.broadcastTime + "ms")];
+                                let ranks = [(lastProcessedClaim.username + " claimed after " + (lastProcessedClaim.timestamp - nextDrop.broadcastTime) + "ms")];
                                 // collect claim times
                                 claimBuffer.forEach(claim => {
                                     console.log(" - " + claim.username + ": +" + (claim.timestamp - lastProcessedClaim.timestamp) + "ms");

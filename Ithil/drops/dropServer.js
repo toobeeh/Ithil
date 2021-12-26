8b165ce0a9041e8d3b92202906b10adbfafe6781
tobeh <tobiasch@gmx.at>
2021-12-26 17:04:41 +0100

    test drop server

--- Ithil/drops/dropServer.js
+++ Ithil/drops/dropServer.js
@@ -4,8 +4,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var app = require('express')();
 var workerHttps = require('https');
 var fs = require('fs');
+var cors = require('cors');
 var ipc = require('node-ipc');
 var config = require("../ecosystem.config").config;
 var palantirDb = require("../palantirDatabase");
-console.log("hi");
+var ws_1 = require("ws");
+console.log(config);
+// use cors
+app.use(cors());
+var dropServer = workerHttps.createServer({
+    key: fs.readFileSync(config.certificatePath + '/privkey.pem', 'utf8'),
+    cert: fs.readFileSync(config.certificatePath + '/cert.pem', 'utf8'),
+    ca: fs.readFileSync(config.certificatePath + '/chain.pem', 'utf8')
+}, app);
+var wss = new ws_1.WebSocketServer({ dropServer: dropServer });
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    ws.send('something');
+});
+dropServer.listen(config.dropPort);
 //# sourceMappingURL=dropServer.js.map
--- Ithil/drops/dropServer.ts
+++ Ithil/drops/dropServer.ts
@@ -2,10 +2,29 @@
 const app = require('express')();
 const workerHttps = require('https');
 const fs = require('fs');
-import WebSocket, { WebSocketServer } from 'ws';
+const cors = require('cors');
 const ipc = require('node-ipc');
 const config = require("../ecosystem.config").config;
 const palantirDb = require("../palantirDatabase");
+import WebSocket, { WebSocketServer } from 'ws';
 
-console.log("hi");
+console.log(config);
+
+// use cors
+app.use(cors());
+const dropServer = workerHttps.createServer({ // create server
+    key: fs.readFileSync(config.certificatePath + '/privkey.pem', 'utf8'),
+    cert: fs.readFileSync(config.certificatePath + '/cert.pem', 'utf8'),
+    ca: fs.readFileSync(config.certificatePath + '/chain.pem', 'utf8')
+}, app);
+const wss = new WebSocketServer({ dropServer });
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+});
+
+dropServer.listen(config.dropPort);
 

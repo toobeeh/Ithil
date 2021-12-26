201b5e4f9a517c1b3a1ce588930c07137f0fb5e3
tobeh <tobiasch@gmx.at>
2021-12-26 16:55:03 +0100

    test require in ts and import

--- Ithil/drops/dropServer.js
+++ Ithil/drops/dropServer.js
@@ -1,2 +1,11 @@
-console.warn("hello there");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// require packets
+var app = require('express')();
+var workerHttps = require('https');
+var fs = require('fs');
+var ipc = require('node-ipc');
+var config = require("../ecosystem.config").config;
+var palantirDb = require("../palantirDatabase");
+console.log("hi");
 //# sourceMappingURL=dropServer.js.map
--- Ithil/drops/dropServer.ts
+++ Ithil/drops/dropServer.ts
@@ -1,1 +1,11 @@
-console.warn("hello there");
+// require packets
+const app = require('express')();
+const workerHttps = require('https');
+const fs = require('fs');
+import WebSocket, { WebSocketServer } from 'ws';
+const ipc = require('node-ipc');
+const config = require("../ecosystem.config").config;
+const palantirDb = require("../palantirDatabase");
+
+console.log("hi");
+
